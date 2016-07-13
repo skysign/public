7b399cc8e8c6734f934e6b2efea979346674731b
ByeongKeon Kim <skysign@ip-172-31-13-56.ap-northeast-2.compute.internal>
2016-07-13 20:02:53 +0900

    WebsiteTest: two updates
    java version is set to 1.8
    chromedriver is added

--- WebsiteTest/SkysignTistoryCom/src/test/java/SkysignTistoryComTestng.java
+++ WebsiteTest/SkysignTistoryCom/src/test/java/SkysignTistoryComTestng.java
@@ -5,8 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+//import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class SkysignTistoryComTestng {
@@ -17,8 +18,9 @@ public class SkysignTistoryComTestng {
 
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
-    //driver = new FirefoxDriver();
-    driver = new InternetExplorerDriver();
+    driver = new ChromeDriver();	
+	//driver = new FirefoxDriver();
+    //driver = new InternetExplorerDriver();
     baseUrl = "https://www.google.co.kr/";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     driver.manage().window().maximize();
